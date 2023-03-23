a515c86eafbb8db912aafba7af7e5aa9f3f94a0c
ExponentialML <59846140+ExponentialML@users.noreply.github.com>
2023-03-22 23:46:57 -0700

    Add training script

---
+++ train.py
@@ -0,0 +1,488 @@
+import argparse
+import datetime
+import logging
+import inspect
+import math
+import os
+import random
+import gc
+
+from typing import Dict, Optional, Tuple
+from omegaconf import OmegaConf
+
+import torch
+import torch.nn.functional as F
+import torch.utils.checkpoint
+import torchvision.transforms as T
+import diffusers
+import transformers
+
+from torchvision import transforms
+from tqdm.auto import tqdm
+
+from accelerate import Accelerator
+from accelerate.logging import get_logger
+from accelerate.utils import set_seed
+
+
+from diffusers.models import AutoencoderKL, UNet3DConditionModel
+from diffusers import DPMSolverMultistepScheduler, DDPMScheduler, TextToVideoSDPipeline
+from diffusers.optimization import get_scheduler
+from diffusers.utils import check_min_version, export_to_video
+from diffusers.utils.import_utils import is_xformers_available
+
+
+from transformers import CLIPTextModel, CLIPTokenizer
+from utils.dataset import VideoDataset
+from einops import rearrange, repeat
+
+already_printed_unet = False
+
+# Will error if the minimal version of diffusers is not installed. Remove at your own risks.
+check_min_version("0.10.0.dev0")
+
+logger = get_logger(__name__, log_level="INFO")
+
+def create_logging(logging, logger, accelerator):
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+    logger.info(accelerator.state, main_process_only=False)
+
+def accelerate_set_verbose(accelerator):
+    if accelerator.is_local_main_process:
+        transformers.utils.logging.set_verbosity_warning()
+        diffusers.utils.logging.set_verbosity_info()
+    else:
+        transformers.utils.logging.set_verbosity_error()
+        diffusers.utils.logging.set_verbosity_error()
+
+def create_output_folders(output_dir, config):
+    now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    out_dir = os.path.join(output_dir, f"train_{now}")
+    
+    os.makedirs(out_dir, exist_ok=True)
+    os.makedirs(f"{out_dir}/samples", exist_ok=True)
+    OmegaConf.save(config, os.path.join(out_dir, 'config.yaml'))
+
+    return out_dir
+
+def load_primary_models(pretrained_model_path):
+    noise_scheduler = DDPMScheduler.from_pretrained(pretrained_model_path, subfolder="scheduler")
+    tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_path, subfolder="tokenizer")
+    text_encoder = CLIPTextModel.from_pretrained(pretrained_model_path, subfolder="text_encoder")
+    vae = AutoencoderKL.from_pretrained(pretrained_model_path, subfolder="vae")
+    unet = UNet3DConditionModel.from_pretrained(pretrained_model_path, subfolder="unet")
+
+    return noise_scheduler, tokenizer, text_encoder, vae, unet
+
+
+def freeze_models(models_to_freeze):
+    for model in models_to_freeze:
+        if model is not None: model.requires_grad_(False) 
+
+def handle_xformers(enable_xformers_memory_efficient_attention, unet):
+    if enable_xformers_memory_efficient_attention:
+        if is_xformers_available():
+            unet.enable_xformers_memory_efficient_attention()
+        else:
+            raise ValueError("xformers is not available. Make sure it is installed correctly")
+
+def param_optim(model, condition):
+    return {"model": model, "condition": condition}
+
+def create_optimizer_params(model_list, lr):
+    optimizer_params = []
+
+    for optim in model_list:
+        # If this is true, we can train it.
+        if optim['condition']:
+            optimizer_params.append({
+                "params": optim['model'].parameters(), "lr": lr
+            })
+    
+    return optimizer_params
+
+def get_optimizer(use_8bit_adam):
+    if use_8bit_adam:
+        try:
+            import bitsandbytes as bnb
+        except ImportError:
+            raise ImportError(
+                "Please install bitsandbytes to use 8-bit Adam. You can do so by running `pip install bitsandbytes`"
+            )
+
+        return bnb.optim.AdamW8bit
+    else:
+        return torch.optim.AdamW
+
+def is_mixed_precision(accelerator):
+    weight_dtype = torch.float32
+
+    if accelerator.mixed_precision == "fp16":
+        weight_dtype = torch.float16
+
+    elif accelerator.mixed_precision == "bf16":
+        weight_dtype = torch.bfloat16
+
+    return weight_dtype
+
+def cast_to_gpu_and_type(model_list, accelerator, weight_dtype):
+    for model in model_list:
+        if model is not None: model.to(accelerator.device, dtype=weight_dtype)
+
+def enable_trainable_unet_modules(model, trainable_modules=None, is_enabled=True):
+    global already_printed_unet
+
+    # This can most definitely be refactored :-)
+    unfrozen_params = 0
+    if trainable_modules is not None:
+        for name, module in model.named_modules():
+            for tm in tuple(trainable_modules):
+                if tm in name:
+                    for m in module.parameters():
+                        m.requires_grad_(is_enabled)
+                        if is_enabled: unfrozen_params +=1
+
+    if unfrozen_params > 0 and not already_printed_unet:
+        already_printed_unet = True 
+        print(f"{unfrozen_params} params have been unfrozen for training.")
+
+def tensor_to_vae_latent(t, vae):
+    video_length = t.shape[1]
+
+    t = rearrange(t, "b f c h w -> (b f) c h w")
+    latents = vae.encode(t).latent_dist.sample()
+    latents = rearrange(latents, "(b f) c h w -> b c f h w", f=video_length)
+    latents = latents * 0.18215
+
+    return latents
+
+def should_sample(global_step, validation_steps, validation_data):
+    return (global_step % validation_steps == 0 or global_step == 1)  \
+    and validation_data.sample_preview
+
+def replace_prompt(prompt, token, wlist):
+    for w in wlist:
+        if w in prompt: return prompt.replace(w, token)
+    return prompt 
+
+def main(
+    pretrained_model_path: str,
+    output_dir: str,
+    train_data: Dict,
+    validation_data: Dict,
+    validation_steps: int = 100,
+    trainable_modules: Tuple[str] = ("attn1", "attn2" ),
+    train_batch_size: int = 1,
+    max_train_steps: int = 500,
+    learning_rate: float = 5e-5,
+    scale_lr: bool = False,
+    lr_scheduler: str = "constant",
+    lr_warmup_steps: int = 0,
+    adam_beta1: float = 0.9,
+    adam_beta2: float = 0.999,
+    adam_weight_decay: float = 1e-2,
+    adam_epsilon: float = 1e-08,
+    max_grad_norm: float = 1.0,
+    gradient_accumulation_steps: int = 1,
+    checkpointing_steps: int = 500,
+    resume_from_checkpoint: Optional[str] = None,
+    mixed_precision: Optional[str] = "fp16",
+    use_8bit_adam: bool = False,
+    enable_xformers_memory_efficient_attention: bool = True,
+    seed: Optional[int] = None,
+    train_text_encoder: bool = False,
+    **kwargs
+):
+
+
+    *_, config = inspect.getargvalues(inspect.currentframe())
+
+    accelerator = Accelerator(
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        mixed_precision=mixed_precision,
+        log_with="tensorboard",
+        logging_dir=output_dir
+    )
+
+    # Make one log on every process with the configuration for debugging.
+    create_logging(logging, logger, accelerator)
+
+    # Initialize accelerate, transformers, and diffusers warnings
+    accelerate_set_verbose(accelerator)
+
+    # If passed along, set the training seed now.
+    if seed is not None:
+        set_seed(seed)
+
+    # Handle the output folder creation
+    if accelerator.is_main_process:
+       output_dir = create_output_folders(output_dir, config)
+
+    # Load scheduler, tokenizer and models.
+    noise_scheduler, tokenizer, text_encoder, vae, unet = load_primary_models(pretrained_model_path)
+
+    # Freeze any necessary models
+    freeze_models([vae, text_encoder.text_model.encoder, unet])
+    
+    # Enable xformers if available
+    #handle_xformers(enable_xformers_memory_efficient_attention, unet)
+
+    if scale_lr:
+        learning_rate = (
+            learning_rate * gradient_accumulation_steps * train_batch_size * accelerator.num_processes
+        )
+
+    # Initialize the optimizer
+    optimizer_cls = get_optimizer(use_8bit_adam)
+
+    # Create parameters to optimize over with a condition (if "condition" is true, optimize it)
+    optim_params = [
+        param_optim(unet, trainable_modules is not None),
+        param_optim(text_encoder.text_model.encoder.parameters, train_text_encoder == True),
+    ]
+    params = create_optimizer_params(optim_params, learning_rate)
+    
+    # Create Optimizer
+    optimizer = optimizer_cls(
+        params,
+        lr=learning_rate,
+        betas=(adam_beta1, adam_beta2),
+        weight_decay=adam_weight_decay,
+        eps=adam_epsilon,
+    )
+
+    # Scheduler
+    lr_scheduler = get_scheduler(
+        lr_scheduler,
+        optimizer=optimizer,
+        num_warmup_steps=lr_warmup_steps * gradient_accumulation_steps,
+        num_training_steps=max_train_steps * gradient_accumulation_steps,
+    )
+
+    # Get the training dataset
+    train_dataset = VideoDataset(**train_data, tokenizer=tokenizer)
+
+    # DataLoaders creation:
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=train_batch_size, shuffle=True
+    )
+
+    # Used for unconditional training. Not implemented in training config, but may do so manually.
+    uncond_ids = tokenizer(
+                "",
+                truncation=True,
+                padding="max_length",
+                max_length=tokenizer.model_max_length,
+                return_tensors="pt",
+    ).input_ids.to(accelerator.device)
+
+    # Prepare everything with our `accelerator`.
+    unet, optimizer,train_dataloader, lr_scheduler, text_encoder.text_model.encoder = accelerator.prepare(
+        unet, 
+        optimizer, 
+        train_dataloader, 
+        lr_scheduler, 
+        text_encoder.text_model.encoder
+    )
+
+    # For mixed precision training we cast the text_encoder and vae weights to half-precision
+    # as these models are only used for inference, keeping weights in full precision is not required.
+    weight_dtype = is_mixed_precision(accelerator)
+
+    # Move text encoders, and VAE to GPU
+    models_to_cast = [text_encoder, vae]
+    cast_to_gpu_and_type(models_to_cast, accelerator, weight_dtype)
+
+    # We need to recalculate our total training steps as the size of the training dataloader may have changed.
+    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / gradient_accumulation_steps)
+
+    # Afterwards we recalculate our number of training epochs
+    num_train_epochs = math.ceil(max_train_steps / num_update_steps_per_epoch)
+
+    # We need to initialize the trackers we use, and also store our configuration.
+    # The trackers initializes automatically on the main process.
+    if accelerator.is_main_process:
+        accelerator.init_trackers("text2video-fine-tune")
+
+    # Train!
+    total_batch_size = train_batch_size * accelerator.num_processes * gradient_accumulation_steps
+
+    logger.info("***** Running training *****")
+    logger.info(f"  Num examples = {len(train_dataset)}")
+    logger.info(f"  Num Epochs = {num_train_epochs}")
+    logger.info(f"  Instantaneous batch size per device = {train_batch_size}")
+    logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
+    logger.info(f"  Gradient Accumulation steps = {gradient_accumulation_steps}")
+    logger.info(f"  Total optimization steps = {max_train_steps}")
+    global_step = 0
+    first_epoch = 0
+
+    # Potentially load in the weights and states from a previous save
+    if resume_from_checkpoint:
+        if resume_from_checkpoint != "latest":
+            path = os.path.basename(resume_from_checkpoint)
+        else:
+            # Get the most recent checkpoint
+            dirs = os.listdir(output_dir)
+            dirs = [d for d in dirs if d.startswith("checkpoint")]
+            dirs = sorted(dirs, key=lambda x: int(x.split("-")[1]))
+            path = dirs[-1]
+        accelerator.print(f"Resuming from checkpoint {path}")
+        accelerator.load_state(os.path.join(output_dir, path))
+        global_step = int(path.split("-")[1])
+
+        first_epoch = global_step // num_update_steps_per_epoch
+        resume_step = global_step % num_update_steps_per_epoch
+
+    # Only show the progress bar once on each machine.
+    progress_bar = tqdm(range(global_step, max_train_steps), disable=not accelerator.is_local_main_process)
+    progress_bar.set_description("Steps")
+
+    def finetune_unet(batch, train_encoder=False):
+
+        # Set noise scheduler to cosine (this can be done via config, but this ensures it's enabled)
+        #noise_scheduler.beta_schedule = "squaredcos_cap_v2"
+
+        # Convert videos to latent space
+        pixel_values = batch["pixel_values"].to(weight_dtype)
+
+        latents = tensor_to_vae_latent(pixel_values, vae)
+
+        # Sample noise that we'll add to the latents
+        noise = torch.randn_like(latents)
+        bsz = latents.shape[0]
+
+        # Sample a random timestep for each video
+        timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, (bsz,), device=latents.device)
+        timesteps = timesteps.long()
+
+        # Add noise to the latents according to the noise magnitude at each timestep
+        # (this is the forward diffusion process)
+        noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
+        
+        # Enable text encoder training
+        if train_encoder:
+            text_encoder.train()
+
+        enable_trainable_unet_modules(unet, trainable_modules, is_enabled=True)
+
+        # Get the text embedding for conditioning
+        encoder_hidden_states = text_encoder(batch['prompt_ids'])[0]
+
+        # Get the target for loss depending on the prediction type
+        if noise_scheduler.prediction_type == "epsilon":
+            target = noise
+        elif noise_scheduler.prediction_type == "v_prediction":
+            target = noise_scheduler.get_velocity(latents, noise, timesteps)
+        else:
+            raise ValueError(f"Unknown prediction type {noise_scheduler.prediction_type}")
+
+        
+        # Predict the noise residual and compute loss
+        model_pred = unet(noisy_latents, timesteps, encoder_hidden_states=encoder_hidden_states).sample
+        loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
+        return loss, latents
+
+    for epoch in range(first_epoch, num_train_epochs):
+        train_loss = 0.0
+        unet.train()
+        for step, batch in enumerate(train_dataloader):
+            # Skip steps until we reach the resumed step
+            if resume_from_checkpoint and epoch == first_epoch and step < resume_step:
+                if step % gradient_accumulation_steps == 0:
+                    progress_bar.update(1)
+                continue
+            
+            with accelerator.accumulate(unet) ,accelerator.accumulate(text_encoder.text_model.encoder):
+
+                text_prompt = batch['text_prompt'][0]
+                pixel_values = batch["pixel_values"].to(weight_dtype)
+                video_length = pixel_values.shape[1]
+
+                with accelerator.autocast():
+                    loss, latents = finetune_unet(batch, train_encoder=train_text_encoder)
+                
+                # Gather the losses across all processes for logging (if we use distributed training).
+                avg_loss = accelerator.gather(loss.repeat(train_batch_size)).mean()
+                train_loss += avg_loss.item() / gradient_accumulation_steps
+
+                # Backpropagate
+                try:
+                    accelerator.backward(loss)
+                    if accelerator.sync_gradients:
+                        accelerator.clip_grad_norm_(filter(lambda p: p.requires_grad, unet.parameters()), max_grad_norm)
+                
+                    optimizer.step()
+                    lr_scheduler.step()
+                    optimizer.zero_grad()
+                    
+                except Exception as e:
+                    print(f"An error has occured during backpropogation! {e}") 
+                    continue
+
+            # Checks if the accelerator has performed an optimization step behind the scenes
+            if accelerator.sync_gradients:
+                progress_bar.update(1)
+                global_step += 1
+                accelerator.log({"train_loss": train_loss}, step=global_step)
+                train_loss = 0.0
+            
+                if global_step % checkpointing_steps == 0:
+                    save_path = os.path.join(output_dir, f"checkpoint-{global_step}")
+                    accelerator.save_state(save_path)
+                    logger.info(f"Saved state to {save_path}")
+
+                if should_sample(global_step, validation_steps, validation_data):
+                    if global_step == 1: print("Performing validation prompt.")
+                    if accelerator.is_main_process:
+                        with accelerator.autocast():
+
+                            pipeline = TextToVideoSDPipeline.from_pretrained(
+                                pretrained_model_path,
+                                text_encoder=text_encoder,
+                                vae=vae,
+                                unet=unet
+                            )
+                            diffusion_scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
+                            pipeline.scheduler = diffusion_scheduler
+
+                            out_file = f"{output_dir}/samples/{text_prompt}_{global_step}.mp4"
+                            video_frames = pipeline(text_prompt).frames
+                            video_path = export_to_video(video_frames, out_file)
+                            del pipeline
+                            gc.collect()
+
+                    logger.info(f"Saved a new sample to {out_file}")
+
+            logs = {"step_loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
+            accelerator.log({"training_loss": loss.detach().item()}, step=step)
+            progress_bar.set_postfix(**logs)
+
+            if global_step >= max_train_steps:
+                break
+
+    # Create the pipeline using the trained modules and save it.
+    accelerator.wait_for_everyone()
+    if accelerator.is_main_process:
+
+        unet = accelerator.unwrap_model(unet)
+        pipeline = TextToVideoSDPipeline.from_pretrained(
+            pretrained_model_path,
+            text_encoder=text_encoder,
+            vae=vae,
+            unet=unet,
+        )
+        pipeline.save_pretrained(output_dir)
+            
+    accelerator.end_training()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default="./configs/my_config.yaml")
+    args = parser.parse_args()
+
+    main(**OmegaConf.load(args.config))
