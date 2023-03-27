c8b75ad6f484c4403113a5784af49f7e7f1167c6
ExponentialML <exponentialservices@proton.me>
2023-03-26 17:32:02 -0700

    Add global unfrozen var

--- train.py
+++ train.py
@@ -36,6 +36,7 @@ from utils.dataset import VideoDataset
 from einops import rearrange, repeat
 
 already_printed_unet = False
+already_unfrozen = False
 
 # Will error if the minimal version of diffusers is not installed. Remove at your own risks.
 check_min_version("0.10.0.dev0")
