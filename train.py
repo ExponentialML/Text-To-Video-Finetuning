ec65f4c0c4f4155a0aad8ffc3698db1df1234f1c
ExponentialML <59846140+ExponentialML@users.noreply.github.com>
2023-08-08 17:03:02 -0700

    Update train.py

--- train.py
+++ train.py
@@ -18,7 +18,6 @@ import torch.utils.checkpoint
 import torchvision.transforms as T
 import diffusers
 import transformers
-import itertools
 
 from torchvision import transforms
 from tqdm.auto import tqdm
@@ -845,12 +844,13 @@ def main(
                 # Backpropagate
                 try:
                     accelerator.backward(loss)
-                    params_to_clip = (
-                        [unet.parameters()] if (not train_text_encoder or not use_text_lora) 
-                    else 
-                        list(unet.parameters()) + list(text_encoder.parameters())
-                    )
-                    accelerator.clip_grad_norm_(itertools.chain(*params_to_clip), max_grad_norm)
+
+                    if any([train_text_encoder, use_text_lora]):
+                        params_to_clip = list(unet.parameters()) + list(text_encoder.parameters())
+                    else:
+                        params_to_clip = unet.parameters()
+
+                    accelerator.clip_grad_norm_(params_to_clip, max_grad_norm)
                 
                     optimizer.step()
                     lr_scheduler.step()
