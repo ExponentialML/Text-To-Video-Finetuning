3c16628aa514a3e3d1062a46adf2322244321dba
Xinzhi F <xfanac@stanford.edu>
2023-11-21 00:16:49 -0800

    Update batch inference

--- batch_inference.sh
+++ batch_inference.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-check_points='./outputs/train_2023-11-14T18-55-08/checkpoint-*'
-suffix="unique_token_sks_only"
-prompts=("\"A sks person is playing ball.\"" "\"A sks person is playing basketball.\"" "\"A sks person is playing soccer.\"" "\"A sks person is surfing.\"")
+check_points='./outputs/train_2023-11-20T23-10-32/checkpoint-*'
+suffix=unique_token_dog_only_$(date +%s)
+prompts=("\"A « dog is playing ball\"" "\"A « dog is runnig\"" "\"A dog\"" "\"A dog is running\"")
 for prompt in "${prompts[@]}"
 do
+    per_file_suffix=_"$suffix"_benchmark
+    eval "python inference.py --prompt $prompt --model ./models/model_scope_diffusers --sdp --suffix $per_file_suffix --output-dir ./output/$suffix"
     for checkpoint_file in $check_points
     do
         iteration="${checkpoint_file##*-}"
         per_file_suffix=_"$suffix"_"$iteration"
 
-        eval "python inference.py --prompt $prompt --model $checkpoint_file --sdp --suffix $per_file_suffix"
+        eval "python inference.py --prompt $prompt --model $checkpoint_file --sdp --suffix $per_file_suffix --output-dir ./output/$suffix"
     done
 done
